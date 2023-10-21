2d153b7cbe37d4cbea92afdae4e07c024df7542c
igggys@gmail.com <igggys@gmail.com>
2023-10-21 11:36:03 +0300

    Adding class Connector

--- WebAppTest/Services/Connector.cs
+++ WebAppTest/Services/Connector.cs
@@ -1,4 +1,9 @@
-﻿namespace WebAppTest.Services
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text;
+using System;
+using WebAppTest.Infrastructure;
+
+namespace WebAppTest.Services
 {
     public class Connector
     {
@@ -7,9 +12,33 @@
             //
         }
 
-        public async Task<object> Get()
+        public async Task<ApplicationResponse> Get(string serviceName, string path, Dictionary<string, string> headers)
         {
-            
+            ApplicationResponse result = null;
+
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync(path);
+
+                    if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                    {
+                        result = new() { IsError = true, Message = $"Response StatusCode is {response.StatusCode}", Data = null };
+                    }
+                    else
+                    {
+                        var responseContentString = await response.Content.ReadAsStringAsync();
+                        result = new() { IsError = false, Message = string.Empty, Data = responseContentString };
+                    }
+                }
+                catch (System.Exception exception)
+                {
+                    result = new() { IsError = true, Message = exception.Message, Data = null };
+                }
+            }
+
+            return result;
         }
     }
 }
